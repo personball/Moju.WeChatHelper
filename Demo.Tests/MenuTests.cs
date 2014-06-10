c072e8c7d6d269d1d5bcd54c2fab20511b0828b9
personball <personball@163.com>
2014-06-10 20:39:51 +0800

    添加单元测试，针对菜单的json格式数据生成Menu对象
    测试微信菜单查询接口返回的json数据是否正确生成Menu对象

--- Demo.Tests/MenuTests.cs
+++ Demo.Tests/MenuTests.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Schema;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moju.WeChatHelper;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Linq;
 namespace Demo.Tests
 {
     [TestClass]
@@ -42,7 +41,7 @@ namespace Demo.Tests
                'key':'V1001_GOOD'
             }]
        }]
- }".Replace("\r\n", "").Replace("\t", "").Replace(" ", "").Replace("'","\"");
+ }".Replace("\r\n", "").Replace("\t", "").Replace(" ", "").Replace("'", "\"");
 
             Menu menu = new Menu();
             menu.button = new List<Button>();
@@ -58,6 +57,63 @@ namespace Demo.Tests
 
             string ret = menu.GetJson().ToString().Replace("\r\n", "").Replace("\t", "").Replace(" ", "");
             Assert.AreEqual(except, ret);
+        }
+        [TestMethod]
+        public void Json2Menu()
+        {
+            string jsonData = @"
+{
+    'menu': {
+        'button': [
+            {
+                'type': 'click',
+                'name': '今日歌曲',
+                'key': 'V1001_TODAY_MUSIC',
+                'sub_button': []
+            },
+            {
+                'type': 'click',
+                'name': '歌手简介',
+                'key': 'V1001_TODAY_SINGER',
+                'sub_button': []
+            },
+            {
+                'name': '菜单',
+                'sub_button': [
+                    {
+                        'type': 'view',
+                        'name': '搜索',
+                        'url': 'http://www.soso.com/',
+                        'sub_button': []
+                    },
+                    {
+                        'type': 'view',
+                        'name': '视频',
+                        'url': 'http://v.qq.com/',
+                        'sub_button': []
+                    },
+                    {
+                        'type': 'click',
+                        'name': '赞一下我们',
+                        'key': 'V1001_GOOD',
+                        'sub_button': []
+                    }
+                ]
+            }
+        ]
+    }
+}";
+            JObject Jobj = JObject.Parse(jsonData);
+            Menu menu = new Menu();
+            menu.button = ((JArray)Jobj["menu"]["button"]).ToWeChatMenuButtonList().ToList();
+
+            Assert.IsInstanceOfType(menu.button[0], typeof(ClickButton));
+            Assert.IsInstanceOfType(menu.button[1], typeof(ClickButton));
+            Assert.IsInstanceOfType(menu.button[2], typeof(HasSubButton));
+            HasSubButton sub = (HasSubButton)menu.button[2];
+            Assert.IsInstanceOfType(sub.SubButton[0], typeof(ViewButton));
+            Assert.IsInstanceOfType(sub.SubButton[1], typeof(ViewButton));
+            Assert.IsInstanceOfType(sub.SubButton[2], typeof(ClickButton));
         }
     }
 }
