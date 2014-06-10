f74cb6395988361006f117cdc22eec67ae1c14fa
personball <personball@163.com>
2014-06-10 20:38:04 +0800

    扩展 json.net 库方法
    扩展JObject.ToWeChatMenuButton() 和 JArray.ToWeChatMenuButtonList()
    方法，便于由json数据生成Menu对象

---
+++ Moju.WeChatHelper/Menu/JsonExtend.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+namespace Moju.WeChatHelper
+{
+    public static class JsonExtend
+    {
+        public static Button ToWeChatMenuButton(this JObject JObj)
+        {
+            if (JObj["type"] == null)
+            {
+                return new HasSubButton
+                {
+                    Name = JObj["name"].ToString(),
+                    SubButton = ((JArray)JObj["sub_button"]).ToWeChatMenuButtonList().ToList()
+                };
+            }
+            else
+            {
+                switch (JObj["type"].ToString())
+                {
+                    case "click":
+                        {
+                            return JObj.ToObject<ClickButton>();
+                        }
+                    case "view":
+                        {
+                            return JObj.ToObject<ViewButton>();
+                        }
+                    default:
+                        {
+                            return null;
+                        }
+                }
+            }
+
+        }
+        public static IEnumerable<Button> ToWeChatMenuButtonList(this JArray JArr)
+        {
+            return from obj in JArr
+                   select ((JObject)obj).ToWeChatMenuButton();
+        }
+    }
+}
