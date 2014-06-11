bcaf71a80b3627b89741a12cafccaf0c80a846d2
personball <personball@163.com>
2014-06-11 13:04:27 +0800

    为Demo提取常用方法
    GetToken 和 Log 方法，从控制器中分离出来

---
+++ Demo/Models/Tools.cs
@@ -0,0 +1,32 @@
+﻿using Moju.WeChatHelper;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Web;
+namespace Demo.Models
+{
+    public class Tools
+    {
+        public static string GetToken()
+        {
+            string AccessToken = WeChatHelper.GetAccessToken();//实际开发时，请勿过多请求AccessToken，保存后未过期则沿用
+            //正常结果：{"access_token":"ACCESS_TOKEN","expires_in":7200}
+            //异常结果：{"errcode":40013,"errmsg":"invalid appid"}
+            var tokenObj = new { access_token = "", expires_in = 0 };
+            var tokenRes = JsonConvert.DeserializeAnonymousType(AccessToken, tokenObj);
+            return tokenRes.access_token;
+        }
+        /// <summary>
+        /// 简单日志，调试用
+        /// </summary>
+        /// <param name="str"></param>
+        public static void Log(string str)
+        {
+            using (StreamWriter sw = new StreamWriter(HttpContext.Current.Server.MapPath("~/" + DateTime.Now.ToString("yyyyMMdd") + ".log"), true))
+            {
+                sw.WriteLine("{0}\t :{1}", DateTime.Now, str);
+            }
+        }
+
+    }
+}
