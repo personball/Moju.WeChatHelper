5cd1f2dd55f52f4c0e92d14d8f3c6b6574d11911
personball <personball@163.com>
2014-06-11 13:05:18 +0800

    菜单相关线上测试示例独立为一个Menu控制器
    菜单相关线上测试示例独立为一个Menu控制器

--- Demo/Controllers/HomeController.cs
+++ Demo/Controllers/HomeController.cs
@@ -1,13 +1,9 @@
-﻿using Moju.WeChatHelper;
+﻿using Demo.Models;
+using Moju.WeChatHelper;
+using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Xml.Linq;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 namespace Demo.Controllers
 {
     public class HomeController : Controller
@@ -32,7 +28,7 @@ namespace Demo.Controllers
         public ActionResult WeChat()
         {
             XDocument xml = XDocument.Load(Request.InputStream);
-            log(xml.ToString(SaveOptions.DisableFormatting));//log
+            Tools.Log(xml.ToString(SaveOptions.DisableFormatting));//log
             //xml反序列化为对象
             Message msg = MessageFactory.CreateFromXml(xml);
             //业务处理，根据自己的业务规则进行处理
@@ -43,7 +39,7 @@ namespace Demo.Controllers
                 if (text.Content == "t1")
                 {
                     string AccessToken = WeChatHelper.GetAccessToken();
-                    log("AccessToken:" + AccessToken);
+                    Tools.Log("AccessToken:" + AccessToken);
                     return new WeChatTextResult
                     {
                         FromUserName = text.ToUserName,
@@ -55,7 +51,7 @@ namespace Demo.Controllers
                 //测试图片上传接口
                 if (text.Content == "t2")
                 {
-                    string res = WeChatHelper.UpLoadMediaFile(GetToken(), UpLoadMediaType.Image, Server.MapPath("~/mind.jpg"));
+                    string res = WeChatHelper.UpLoadMediaFile(Tools.GetToken(), UpLoadMediaType.Image, Server.MapPath("~/mind.jpg"));
                     var rightDef = new { type = "", media_id = "", created_at = 0 };//{"type":"TYPE","media_id":"MEDIA_ID","created_at":123456789}
 
                     var resObj = JsonConvert.DeserializeAnonymousType(res, rightDef);
@@ -84,9 +80,9 @@ namespace Demo.Controllers
                 {
                     RImageMessage img = (RImageMessage)msg;
                     //下载图片
-                    string FilePath = WeChatHelper.DownLoadMediaFile(GetToken(), img.MediaId, Server.MapPath("~/files/"));
+                    string FilePath = WeChatHelper.DownLoadMediaFile(Tools.GetToken(), img.MediaId, Server.MapPath("~/files/"));
 
-                    log("download:" + FilePath);
+                    Tools.Log("download:" + FilePath);
 
                     return new WeChatTextResult
                     {
@@ -96,59 +92,8 @@ namespace Demo.Controllers
                         CreateTime = DateTime.Now
                     };
                 }
-
-
                 return Content("");
             }
-        }
-        /// <summary>
-        /// 简单日志，调试用
-        /// </summary>
-        /// <param name="str"></param>
-        private void log(string str)
-        {
-            using (StreamWriter sw = new StreamWriter(Server.MapPath("~/" + DateTime.Now.ToString("yyyyMMdd") + ".log"), true))
-            {
-                sw.WriteLine("{0}\t :{1}", DateTime.Now, str);
-            }
-        }
-
-        public ActionResult SetMenu()
-        {
-            string token = GetToken();
-
-            Menu menu = new Menu();
-            menu.button = new List<Button>();
-            menu.button.Add(new ClickButton { Key = "K001", Name = "测试菜单1" });
-            menu.button.Add(new ClickButton { Key = "K002", Name = "测试菜单2" });
-            menu.button.Add(new ClickButton { Key = "K003", Name = "测试菜单3" });
-
-            string res = WeChatHelper.MenuCreate(token, menu);
-            return Content("done:" + res);
-        }
-
-        public ActionResult GetMenu()
-        {
-            string token = GetToken();
-            string res = WeChatHelper.MenuGet(token);
-            Menu menu = new Menu();
-            if (res!="error")
-            {
-                JObject Jobj = JObject.Parse(res);   
-                menu.button = ((JArray)Jobj["menu"]["button"]).ToWeChatMenuButtonList().ToList();
-            }
-            return Content(res+"<br/>"+menu.GetJson().ToString());
-        }
-
-
-        private string GetToken()
-        {
-            string AccessToken = WeChatHelper.GetAccessToken();//实际开发时，请勿过多请求AccessToken，保存后未过期则沿用
-            //正常结果：{"access_token":"ACCESS_TOKEN","expires_in":7200}
-            //异常结果：{"errcode":40013,"errmsg":"invalid appid"}
-            var tokenObj = new { access_token = "", expires_in = 0 };
-            var tokenRes = JsonConvert.DeserializeAnonymousType(AccessToken, tokenObj);
-            return tokenRes.access_token;
         }
     }
 }
---
+++ Demo/Controllers/MenuController.cs
@@ -0,0 +1,45 @@
+﻿using Demo.Models;
+using Moju.WeChatHelper;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class MenuController : Controller
+    {
+        public ActionResult Set()
+        {
+            string token = Tools.GetToken();
+
+            Menu menu = new Menu();
+            menu.button = new List<Button>();
+            menu.button.Add(new ClickButton { Key = "K001", Name = "测试菜单1" });
+            menu.button.Add(new ClickButton { Key = "K002", Name = "测试菜单2" });
+            menu.button.Add(new ClickButton { Key = "K003", Name = "测试菜单3" });
+
+            string res = WeChatHelper.MenuCreate(token, menu);
+            return Content("done:" + res);
+        }
+
+        public ActionResult Get()
+        {
+            string token = Tools.GetToken();
+            string res = WeChatHelper.MenuGet(token);
+            Menu menu = new Menu();
+            if (res != "error")
+            {
+                JObject Jobj = JObject.Parse(res);
+                menu.button = ((JArray)Jobj["menu"]["button"]).ToWeChatMenuButtonList().ToList();
+            }
+            return Content(res + "<br/>" + menu.GetJson().ToString());
+        }
+        public ActionResult Del()
+        {
+            string token = Tools.GetToken();
+            string res = WeChatHelper.MenuDelete(token);
+            return Content(res);
+        }
+    }
+}
